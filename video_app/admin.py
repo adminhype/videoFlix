c072439f99a46e79a846164875939dbe67c549b3
adminhype <ado.designs@outlook.de>
2026-01-26 04:52:48 +0100

    feat: video and admin model

--- video_app/admin.py
+++ video_app/admin.py
@@ -1,3 +1,24 @@
+from unfold.admin import ModelAdmin
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Video
+
+
+@admin.register(Video)
+class VideoAdmin(ModelAdmin):
+    list_display = [
+        "title",
+        "category",
+        "created_at",
+        "has_720p",
+        "has_1080p"
+    ]
+    search_fields = ["title", "description"]
+    list_filter = ["category", "created_at"]
+
+    fieldsets = (
+        (None, {"fields": ("title", "description", "category")}),
+        ("Media", {"fields": ("video_file", "thumbnail")}),
+        ("Conversion Status", {"fields": ("has_720p", "has_1080p")}),
+    )
--- video_app/models.py
+++ video_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Video(models.Model):
+    class VideoCategory(models.TextChoices):
+        DRAMA = "Drama", _("Drama")
+        COMEDY = "comedy", _("Comedy")
+        DOCUMENTARY = "documentary", _("Documentary")
+        ACTION = "Action", _("Action")
+        HORROR = "Horror", _("Horror")
+        SCI_FI = "Sci-Fi", _("Sci-Fi")
+        ROMANCE = "romance", _("Romance")
+
+    created_at = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=112)
+    description = models.TextField(blank=True, null=True)
+    video_file = models.FileField(upload_to="videos/", blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="thumbnails/", blank=True, null=True)
+    category = models.CharField(
+        max_length=55,
+        choices=VideoCategory.choices,
+        default=VideoCategory.DRAMA
+    )
+    has_720p = models.BooleanField(default=False)
+    has_1080p = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
